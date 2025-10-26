d0722fd86b7a528c901f784d9738d41dc3b25a1c
Daniel Crosby <danielcrosby844@gmail.com>
2025-10-26 13:50:20 +0100

    Validate request, upload avatar, and traceability

--- lambdas/players-create/src/app.py
+++ lambdas/players-create/src/app.py
@@ -1,50 +1,100 @@
+import base64
 import json
 import os
 import boto3
 import pymysql
+from datetime import datetime
 
-# s3_client = boto3.client("s3")
+s3 = boto3.client("s3")
 
-# SQL query
 INSERT_PLAYER_SQL = """
     INSERT INTO players (name, avatar_url, discord_id, team_id, team_role)
     VALUES (%s, %s, %s, %s, %s)
 """
 
-def lambda_handler(event, context):
-    # TODO: Validation on these
-    name = event.get("name")
-    avatar_bytes = event.get("avatar_bytes")
-    discord_id = event.get("discord_id")
-    team_id = event.get("team_id")
-    team_role = event.get("team_role")
+def validate_player_data(player_data) -> bool:
+    if not all(player_data.get(field, "").strip() for field in ["name", "discord_id", "team_role"]):
+        return False
+    
+    try:
+        int(player_data.get("team_id", ""))
+    except (ValueError, TypeError):
+        return False
 
+    return True
+
+def create_connection() -> pymysql.Connection:
+    return pymysql.connect(
+        host=os.environ["DB_HOST"],
+        port=int(os.environ["DB_PORT"]),
+        user=os.environ["DB_USER"],
+        password=os.environ["DB_PASSWORD"],
+        database=os.environ["DB_NAME"],
+        cursorclass=pymysql.cursors.DictCursor
+    )
+
+def upload_avatar(avatar_bytes) -> str | None:
+    if not avatar_bytes:
+        return None
+    
+    decoded_bytes = base64.b64decode(avatar_bytes)
+
+    timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
+
+    file_name = f"avatars/{timestamp}.png"
+
+    s3.put_object(
+        Bucket=os.environ["BUCKET_NAME"],
+        Key=file_name,
+        Body=decoded_bytes,
+        ContentType="image/png"
+    )
+
+    return f"https://lockout.nemika.me/{file_name}"
+
+def lambda_handler(event, context):
+
+    request_id = context.aws_request_id
+
+    player_data = json.loads(event["body"])
+
+    print(f"{request_id} Reveived player_data: {str(player_data)}")
+
+    if not validate_player_data(player_data):
+        print(f"{request_id} Invalid player_data for request_id")
+        return {
+            'statusCode': 400,
+        }
+    
+    name = player_data.get("name")
+    avatar_bytes = player_data.get("avatar_bytes", "")
+    discord_id = player_data.get("discord_id")
+    team_id = int(player_data.get("team_id"))
+    team_role = player_data.get("team_role")
+    
     connection = None
 
     try:
-        connection = pymysql.connect(
-            host=os.environ["DB_HOST"],
-            port=int(os.environ["DB_PORT"]),
-            user=os.environ["DB_USER"],
-            password=os.environ["DB_PASSWORD"],
-            database=os.environ["DB_NAME"],
-            cursorclass=pymysql.cursors.DictCursor
-        )
 
-        # TODO: Properly handle file bytes
-        file_name = f"avatars/{name}.txt"
-        # s3_client.put_object(
-        #     Bucket=os.environ["BUCKET_NAME"],
-        #     Key=file_name,
-        #     Body=avatar_bytes
-        # )
+        print(f"{request_id} Connecting to db")
 
-        avatar_url = f"https://lockout.nemika.me/{file_name}"
+        connection = create_connection()
+
+        print(f"{request_id} Connection established, uploading avatar...")
+
+        avatar_url = upload_avatar(avatar_bytes)
+
+        print(f"{request_id} Avatar uploaded, creating player...")
 
         with connection.cursor() as cursor:
-            cursor.execute(INSERT_PLAYER_SQL, (name, avatar_url, discord_id, team_id, team_role))
+            cursor.execute(
+                INSERT_PLAYER_SQL, 
+                (name, avatar_url, discord_id, team_id, team_role)
+            )
             connection.commit()
             insert_id = cursor.lastrowid
+        
+        print(f"{request_id} Player created. New player id: {insert_id}")
 
         return {    
             'statusCode': 200,
@@ -56,10 +106,10 @@ def lambda_handler(event, context):
     }
 
     except Exception as e:
-        print(f"Failed to create player: {name}, Error: {str(e)}")
+        print(f"{request_id} Failed to create player, Error: {str(e)}")
         return {
             "statusCode": 500,
-            "body": json.dumps(f"Failed to create player: {name}, Error: {str(e)}")
+            "body": json.dumps(f"Failed to create player, Error: {str(e)}")
         }
 
     finally:
