ff6fc24094761a9dce17cfa56384a01dbfd5dd4b
Nemika__ <mmnemijus@gmail.com>
2025-10-27 21:47:42 +0200

    Riot accounts manager lambdas (#4)
    * Add riot accounts create lambda
    
    * Add template.yaml
    
    * Fix naming and remove unused env variable
    
    * Fix lambda errors and add riot accounts trigger to only allow one primary account
    
    * Add update lambda
    
    * Add delete lambda
    
    * Fix

--- db_migration/table_riot_accounts.sql
+++ db_migration/table_riot_accounts.sql
@@ -7,3 +7,38 @@ CREATE TABLE IF NOT EXISTS tournament_db.riot_accounts (
         CHECK (is_primary IN ('true', 'false')),
     FOREIGN KEY (player_id) REFERENCES tournament_db.players(id)
 );
+
+DELIMITER $$
+
+CREATE TRIGGER check_single_primary_insert
+BEFORE INSERT ON tournament_db.riot_accounts
+FOR EACH ROW
+BEGIN
+    IF NEW.is_primary = 'true' THEN
+        IF (SELECT COUNT(*) 
+              FROM tournament_db.riot_accounts 
+             WHERE player_id = NEW.player_id 
+               AND is_primary = 'true') > 0 THEN
+            SIGNAL SQLSTATE '45000'
+                SET MESSAGE_TEXT = 'A player can only have one primary account';
+        END IF;
+    END IF;
+END$$
+
+CREATE TRIGGER check_single_primary_update
+BEFORE UPDATE ON tournament_db.riot_accounts
+FOR EACH ROW
+BEGIN
+    IF NEW.is_primary = 'true' AND (OLD.is_primary <> 'true' OR OLD.player_id <> NEW.player_id) THEN
+        IF (SELECT COUNT(*) 
+              FROM tournament_db.riot_accounts 
+             WHERE player_id = NEW.player_id 
+               AND is_primary = 'true' 
+               AND id <> OLD.id) > 0 THEN
+            SIGNAL SQLSTATE '45000'
+                SET MESSAGE_TEXT = 'A player can only have one primary account';
+        END IF;
+    END IF;
+END$$
+
+DELIMITER ;
