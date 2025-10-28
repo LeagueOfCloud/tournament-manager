388ea9e9aa843d559eedc434cd91822d220a1db8
Nemika-Haj <nemika@bytestobits.dev>
2025-10-28 21:53:19 +0200

    Add avatar_url and token to profiles

--- db_migration/table_profiles.sql
+++ db_migration/table_profiles.sql
@@ -1,6 +1,8 @@
 CREATE TABLE IF NOT EXISTS tournament_db.profiles (
     id INT PRIMARY KEY AUTO_INCREMENT,
     discord_id VARCHAR(50) NOT NULL UNIQUE,
+    avatar_url VARCHAR(255),
     type VARCHAR(20) NOT NULL
-        CHECK (type IN ('player', 'admin', 'user'))
+        CHECK (type IN ('player', 'admin', 'user')) DEFAULT("user"),
+    token VARCHAR(255) NOT NULL DEFAULT(UUID())
 );
