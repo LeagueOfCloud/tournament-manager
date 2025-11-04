8eeab4e8820da11273c5e18c3b4687515e5ad45c
MicheleB <bottam8502@gmail.com>
2025-11-04 21:42:58 +0000

    (In theory) made match history fetch lambda (#18)
    * (In theory) made match history fetch lambda
    
    * fix
    
    * Update main.yaml
    
    * fcuk you
    
    * changed :)
    
    * Update cloudformation/lambdas/match-history/create/src/app.py
    
    Co-authored-by: Nemika__ <mmnemijus@gmail.com>
    
    * Update cloudformation/lambdas/match-history/create/src/app.py
    
    Co-authored-by: Nemika__ <mmnemijus@gmail.com>
    
    * Update cloudformation/lambdas/match-history/create/src/app.py
    
    Co-authored-by: Nemika__ <mmnemijus@gmail.com>
    
    * Update cloudformation/lambdas/match-history/create/src/app.py
    
    Co-authored-by: Nemika__ <mmnemijus@gmail.com>
    
    * Update cloudformation/lambdas/match-history/create/src/app.py
    
    Co-authored-by: Nemika__ <mmnemijus@gmail.com>
    
    * Update cloudformation/lambdas/match-history/create/src/app.py
    
    Co-authored-by: Nemika__ <mmnemijus@gmail.com>
    
    * Update cloudformation/lambdas/match-history/create/src/app.py
    
    Co-authored-by: Nemika__ <mmnemijus@gmail.com>
    
    * Update cloudformation/lambdas/match-history/create/src/app.py
    
    Co-authored-by: Nemika__ <mmnemijus@gmail.com>
    
    * Update cloudformation/lambdas/match-history/create/src/app.py
    
    Co-authored-by: Nemika__ <mmnemijus@gmail.com>
    
    * ALL FIX BISMILLAH
    
    * ....
    
    * Update app.py
    
    * Update app.py
    
    * Update app.py
    
    * lol
    
    * Multiple fixes
    
    ---------
    
    Co-authored-by: Nemika__ <mmnemijus@gmail.com>

---
+++ cloudformation/lambdas/match-history/create/src/app.py
@@ -0,0 +1,132 @@
+import base64
+import json
+import os
+import boto3
+import pymysql
+import requests
+import logging
+from datetime import datetime
+
+s3 = boto3.client("s3")
+
+RIOT_API_KEY = os.environ["RIOT_API_KEY"]
+REGION = "europe"
+GET_PLAYER_UUIDS_SQL = """
+SELECT account_puuid
+FROM riot_accounts
+WHERE last_match_history_fetch IS NULL
+    OR last_match_history_fetch < NOW() - INTERVAL 1 WEEK
+ORDER BY last_match_history_fetch IS NOT NULL, last_match_history_fetch DESC
+LIMIT 5;
+"""
+INSERT_MATCH_HISTORY_SQL = """
+    INSERT INTO match_history (match_id)
+    VALUES (%s)
+"""
+UPDATE_LAST_MATCH_HISTORY_FETCH_SQL = """
+UPDATE riot_accounts
+SET last_match_history_fetch = %s
+WHERE account_puuid = %s
+"""
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+def create_connection() -> pymysql.Connection:
+    return pymysql.connect(
+        host=os.environ["DB_HOST"],
+        port=int(os.environ["DB_PORT"]),
+        user=os.environ["DB_USER"],
+        password=os.environ["DB_PASSWORD"],
+        database=os.environ["DB_NAME"],
+        cursorclass=pymysql.cursors.DictCursor
+    )
+
+def fetch_puuids() -> list:
+    connection = None
+    try:
+        connection = create_connection()
+        with connection.cursor() as cursor:
+            cursor.execute(GET_PLAYER_UUIDS_SQL)
+            results = cursor.fetchall()
+            puuids = [row['account_puuid'] for row in results]
+            logger.info(f"Fetched {len(puuids)} PUUIDs from database.")
+            return puuids
+    except Exception as e: 
+        logger.error(f"Error fetching PUUIDs: {str(e)}")
+        return []
+    
+def update_timestamp(puuid):
+    connection = create_connection()
+    with connection.cursor() as cursor:
+        cursor.execute(UPDATE_LAST_MATCH_HISTORY_FETCH_SQL, (datetime.now(), puuid))
+    connection.commit()
+    connection.close()
+
+
+def fetch_match_ids() -> dict:
+    puuids = fetch_puuids()
+    match_ids = []
+    for puuid in puuids:
+        url = f"https://{REGION}.api.riotgames.com/lol/match/v5/matches/by-puuid/{puuid}/ids"
+        headers = {"X-Riot-Token": RIOT_API_KEY}
+
+        try:
+            logger.info(f"Fetching match IDs for {puuid} from {url}")
+            response = requests.get(url, headers=headers, timeout=10)
+
+            if response.status_code == 429:
+                retry_after = response.headers.get("Retry-After", "1")
+                logger.warning(f"Rate limited by Riot API. Retry after {retry_after}s.")
+                break
+            update_timestamp(puuid)
+
+            response.raise_for_status()
+            match_ids += response.json()
+
+        except requests.exceptions.RequestException as e:
+            logger.error(f"Error fetching match Match ID: {str(e)}")
+            break
+    return match_ids
+
+def lambda_handler(event, context):
+    request_id = context.aws_request_id
+    logger.info(f"Request ID: {request_id}")
+
+    
+    try:
+        match_ids = fetch_match_ids()
+    except Exception as e:
+        logger.error(f"Failed to fetch match IDs: {str(e)}")
+        return {
+            "statusCode": 502,
+            "headers": {"Content-Type": "application/json", "Access-Control-Allow-Origin": "*"},
+            "body": json.dumps({"message": f"Error fetching from Riot API: {str(e)}"})
+        }
+
+    connection = create_connection()
+
+    for match_id in match_ids:
+        try:
+            with connection.cursor() as cursor:
+                cursor.execute(INSERT_MATCH_HISTORY_SQL, (match_id))
+            connection.commit()
+            logger.info(f"Match history for {match_id} inserted successfully.")
+
+
+        except Exception as e:
+            error_code = e.args[0]
+            if error_code == 1062:
+                pass  # Duplicate entry, ignore
+            else:
+                logger.error(f"Could not insert match_id {match_id} to the database: {e}")
+
+    connection.close()
+
+    return {
+        "statusCode": 201,
+        "headers": {
+            "Content-Type": "application/json",
+            "Access-Control-Allow-Origin": "*"
+        },    
+    }
