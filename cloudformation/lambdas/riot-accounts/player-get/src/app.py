2f6deaa1b6677f39138e820a8640635e3198eaca
Nemika-Haj <nemika@bytestobits.dev>
2025-11-05 00:49:51 +0200

    feat(lambda): add GetAccountsFromPlayerIdLambda to retrieve player accounts from the database chore(requirements): add pymysql dependency for database connection in player-get lambda fix(main.yaml): remove unused BUCKET_NAME environment variable from lambda configuration

---
+++ cloudformation/lambdas/riot-accounts/player-get/src/app.py
@@ -0,0 +1,58 @@
+import pymysql
+
+
+def create_connection() -> pymysql.Connection:
+    return pymysql.connect(
+        host=os.environ["DB_HOST"],
+        port=int(os.environ["DB_PORT"]),
+        user=os.environ["DB_USER"],
+        password=os.environ["DB_PASSWORD"],
+        database=os.environ["DB_NAME"],
+        cursorclass=pymysql.cursors.DictCursor,
+    )
+
+
+def lambda_handler(event, context):
+    player_id = event.get("pathParameters", {}).get("player_id")
+
+    if not player_id:
+        return {
+            "statusCode": 400,
+            "headers": {
+                "Content-Type": "application/json",
+                "Access-Control-Allow-Origin": "*",
+            },
+            "body": json.dumps({"message": "Invalid player ID"}),
+        }
+
+    connection = None
+
+    try:
+        connection = create_connection()
+
+        with connection.cursor() as cursor:
+            cursor.execute(
+                "SELECT * FROM riot_accounts WHERE player_id = %s",
+                (player_id)
+            )
+
+            accounts = cursor.fetchall()
+
+        return {
+            "statusCode": 200,
+            "headers": {
+                "Content-Type": "application/json",
+                "Access-Control-Allow-Origin": "*",
+            },
+            "body": json.dumps(accounts),
+        }
+
+    except Exception as e:
+        return {
+            "statusCode": 400,
+            "headers": {
+                "Content-Type": "application/json",
+                "Access-Control-Allow-Origin": "*",
+            },
+            "body": json.dumps({"message": f"{e}"}),
+        }
