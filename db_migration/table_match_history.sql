fdd51fceccf149abb4d198db699515354d58736b
jaimegosai <126070924+JaimeGosai@users.noreply.github.com>
2025-11-05 20:52:19 +0000

    changed match_data type to JSON (#21)

--- db_migration/table_match_history.sql
+++ db_migration/table_match_history.sql
@@ -1,4 +1,7 @@
 CREATE TABLE IF NOT EXISTS tournament_db.match_history (
     match_id VARCHAR(255) PRIMARY KEY,
-    match_data BLOB
+    match_data JSON
 );
+
+ALTER TABLE tournament_db.match_history
+MODIFY match_data JSON;
