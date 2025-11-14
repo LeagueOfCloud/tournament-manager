4315e8e8421ebfb17305884168a3278898bf2dd7
MicheleB <bottam8502@gmail.com>
2025-11-14 16:21:55 +0000

    barebones start for tournament

---
+++ db_migration/table_tournament_matches.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS tournament_db.tournament_matches (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    team_1_id INT NOT NULL,
+    team_2_id INT NOT NULL,
+    start_date DATETIME NOT NULL,
+    end_date DATETIME DEFAULT NULL,
+    winner_team_id INT DEFAULT NULL,
+    tournament_match_id VARCHAR(100) NOT NULL UNIQUE
+);
