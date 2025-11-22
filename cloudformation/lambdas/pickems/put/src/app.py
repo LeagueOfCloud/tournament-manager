2fd9580b6f9f734fb40e9018e54ce88857dbdd3e
Sam Crerar <44707197+Sam-Crerar@users.noreply.github.com>
2025-11-22 11:26:21 +0000

    WIP

---
+++ cloudformation/lambdas/pickems/put/src/app.py
@@ -0,0 +1,208 @@
+import json
+import os
+import pymysql
+import requests
+
+INSERT_PICKEMS_SQL = """
+    INSERT INTO pickems (id, pickems_id, user_id, value)
+    VALUES (%s, %s, %s, %s)
+"""
+
+UPDATE_PICKEMS_SQL = """
+    UPDATE pickems SET value = (%s) where id = (%s)
+""" 
+
+SELECT_PICKEMS_SQL = """
+    SELECT * FROM pickems WHERE id =(%s)
+"""
+
+SELECT_USER_SQL = """
+    SELECT id,type FROM profiles WHERE id = (%s)
+"""
+
+SELECT_CONFIG_SQL = """
+    SELECT value FROM config where name = (%s)
+"""
+SELECT_PLAYER_SQL = """
+    SELECT id FROM players where id = (%s)
+"""
+SELECT_TEAM_SQL = """
+    SELECT id FROM team where id = (%s)
+"""
+
+connection = None
+
+
+def select_pickems(id):
+    with connection.cursor() as cursor:
+        cursor.execute(SELECT_PICKEMS_SQL,
+                       (id))
+    row = cursor.fetchone()
+    return row
+
+def create_pickems(id, pickems_id, user_id, value):
+    with connection.cursor() as cursor:
+        cursor.execute(
+            INSERT_PICKEMS_SQL, 
+            (id, pickems_id, user_id, value)
+        )
+        connection.commit()
+        return cursor.lastrowid 
+    
+def update_pickens(id,value):
+    with connection.cursor() as cursor:
+        cursor.execute(
+             UPDATE_PICKEMS_SQL,
+             (id,value))
+        connection.commit()
+    return cursor.lastrowid 
+             
+
+def create_connection() -> pymysql.Connection:
+    return pymysql.connect(
+        host=os.environ["DB_HOST"],
+        port=int(os.environ["DB_PORT"]),
+        user=os.environ["DB_USER"],
+        password=os.environ["DB_PASSWORD"],
+        database=os.environ["DB_NAME"],
+        cursorclass=pymysql.cursors.DictCursor,
+    )
+
+def pickems_unlocked() -> bool:
+    with connection.cursor() as cursor:
+        cursor.execute(SELECT_CONFIG_SQL,
+                       ("pickem_unlocked"))
+    config = cursor.fetchone()
+    return config[0]
+
+def figure_out_pickems_type(id):
+    with connection.cursor() as cursor:
+        cursor.execute(SELECT_CONFIG_SQL,
+                       ("pickem_categories"))
+    config = cursor.fetchone()
+    jsonconfig = json.loads(config)
+    row = next((item for item in jsonconfig if item["id"] == id), None)
+    row["type"]
+
+
+def valid_user(user_id) -> bool:
+    with connection.cursor() as cursor:
+        cursor.execute(SELECT_USER_SQL,
+                       (user_id))
+    config = cursor.fetchone()
+    if(config.count == 0 ):
+        return False
+    if(config[0].type != "admin"):
+        return False
+    return True
+
+def validate_player(player_value):
+    with connection.cursor() as cursor:
+        cursor.execute(SELECT_PLAYER_SQL,
+                       (player_value))
+    player = cursor.fetchone()
+    if(player is None):
+        return False
+    return True
+
+def validate_team(team_value):
+    with connection.cursor() as cursor:
+        cursor.execute(SELECT_TEAM_SQL,
+                       (team_value))
+    team = cursor.fetchone()
+    if(team is None):
+        return False
+    return True
+
+def validate_champion(champion_value) -> bool:
+    url = "https://ddragon.leagueoflegends.com/cdn/15.23.1/data/en_US/champion.json"
+    response = requests.get(url)
+    response.raise_for_status() 
+    
+    data = response.json()
+    champions = data["data"] 
+    
+    return any(champ["name"].lower() == champion_value.lower() for champ in champions.values())
+
+def lambda_handler(event, context):
+    request_id = context.aws_request_id
+    account_data = json.loads(event["body"])
+    connection = create_connection()
+
+    pickems_id = pickem_data.get("id")
+    #stolen from nemi
+    user_id = event["headers"].get("Authorization") or event["headers"].get("authorization")
+    value = pickem_data.get("value")
+
+
+
+    if not pickems_unlocked() or not valid_user(user_id):     
+        return {
+            "statusCode": 403,
+            "headers": {
+                "Content-Type": "application/json",
+                "Access-Control-Allow-Origin": "*"
+            },
+        }
+    if None in (pickems_id, value):
+        return {
+        "statusCode": 400,
+        "headers": {
+            "Content-Type": "application/json",
+            "Access-Control-Allow-Origin": "*"
+        },
+        "body": json.dumps(f"Invalid data please fix and try again."),
+    }
+
+    pickems_type = figure_out_pickems_type(pickems_id)
+
+    valid = True
+    match pickems_type:
+        case "PLAYER":
+            valid = validate_player(value)
+        case "COUNT":
+            return
+        case "CHAMPION":
+            valid = validate_champion(value)
+        case "TEAM":
+            valid = validate_team(value)
+
+    if(not valid):
+        return {
+        "statusCode": 400,
+        "headers": {
+            "Content-Type": "application/json",
+            "Access-Control-Allow-Origin": "*"
+        },
+        "body": json.dumps(f"Invalid data player/champion/team does not exsist"),
+    }
+
+    try:
+        current_pickem = select_pickems(pickems_id)
+        if(current_pickem is None):
+            if None in (pickems_id, user_id,):
+                return {
+                "statusCode": 400,
+                "headers": {
+                    "Content-Type": "application/json",
+                    "Access-Control-Allow-Origin": "*"
+                },
+                "body": json.dumps(f"Invalid data please fix and try again."),
+            }
+            create_pickems(f"{pickems_id}-{user_id}", pickems_id, user_id, value)    
+        else:
+            update_pickens(f"{pickems_id}-{user_id}",value)
+    except Exception as e:
+        
+        return {
+            "statusCode": 500,
+            "headers": {
+                "Content-Type": "application/json",
+                "Access-Control-Allow-Origin": "*"
+            },
+            "body": json.dumps(f"Failed to create riot account, Error: {str(e)}"),
+        }
+
+    finally:
+        if connection:
+            connection.close()
