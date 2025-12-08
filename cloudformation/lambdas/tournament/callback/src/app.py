aafdb8e0627e048a4b53183f60afc764c45af798
Nemika-Haj <nemika@bytestobits.dev>
2025-12-08 19:18:03 +0200

    Make callback

--- cloudformation/lambdas/tournament/callback/src/app.py
+++ cloudformation/lambdas/tournament/callback/src/app.py
@@ -9,8 +9,8 @@ GET_WINNING_TEAM_SQL = """
 SELECT
     t.id as id
 FROM riot_accounts ra
-JOIN players p WHERE p.id = ra.player_id
-JOIN teams t WHERE t.id = p.team_id
+JOIN players p ON p.id = ra.player_id
+JOIN teams t ON t.id = p.team_id
 WHERE ra.account_puuid = %s
 """
 
@@ -25,7 +25,7 @@ WHERE id = %s
 
 INSERT_MATCH_HISTORY_SQL = """
     INSERT INTO match_history (match_id)
-    VALUES (%s) ON DUPLICATE KEY UPDATE
+    VALUES (%s) ON DUPLICATE KEY UPDATE match_id = match_id
 """
 
 
@@ -40,6 +40,17 @@ def create_connection() -> pymysql.Connection:
     )
 
 
+def response(status_code, body):
+    return {
+        "statusCode": status_code,
+        "headers": {
+            "Content-Type": "application/json",
+            "Access-Control-Allow-Origin": "*",
+        },
+        "body": json.dumps(body),
+    }
+
+
 def lambda_handler(event, context):
     body = json.loads(event["body"])
 
@@ -51,22 +62,36 @@ def lambda_handler(event, context):
         games = res.json()
         connection = create_connection()
 
-        with connection.cursor() as cur:
-            for game in games:
+        for game in games:
+            with connection.cursor() as cur:
                 match_id = f"{game['region']}_{game['gameId']}"
                 tournament_match_id = json.loads(game["metaData"])["id"]
                 winner_puuid = game["winningTeam"][0]["puuid"]
-                cur.execute(GET_WINNING_TEAM_SQL, (winner_puuid, match_id))
+                cur.execute(GET_WINNING_TEAM_SQL, (winner_puuid,))
 
                 winner = cur.fetchone()
                 cur.execute(
                     UPDATE_WINNING_TEAM_SQL,
-                    (winner["id"], datetime.now(), tournament_match_id),
+                    (
+                        winner["id"],
+                        datetime.now(),
+                        match_id,
+                        tournament_match_id,
+                    ),
                 )
 
                 cur.execute(INSERT_MATCH_HISTORY_SQL, (match_id,))
 
         connection.commit()
 
+        return response(200, {"message": "Tournament match result has been processed"})
+
     except Exception as e:
-        traceback.print_exc(e)
+        print(traceback.print_exc())
+        return response(
+            500,
+            {
+                "message": "Could not process tournament match",
+                "error": f"{e}",
+            },
+        )
