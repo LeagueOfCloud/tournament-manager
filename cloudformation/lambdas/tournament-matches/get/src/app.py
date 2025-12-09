c64112d4ba06359adcacfd4d31fd382fc801d839
Nemika-Haj <nemika@bytestobits.dev>
2025-12-09 04:07:51 +0200

    Update tournament matches get

---
+++ cloudformation/lambdas/tournament-matches/get/src/app.py
@@ -0,0 +1,52 @@
+import pymysql
+import json
+import os
+import traceback
+
+SELECT_MATCHES_SQL = """
+SELECT
+    m.*,
+    t1.name as team_1_name,
+    t2.name as team_2_name,
+    t3.name as winner_team_name
+FROM tournament_matches m
+JOIN teams t1 ON t1.id = m.team_1_id
+JOIN teams t2 ON t2.id = m.team_2_id
+JOIN teams t3 ON t3.id = m.winner_team_id
+"""
+
+
+def create_connection() -> pymysql.Connection:
+    return pymysql.connect(
+        host=os.environ["DB_HOST"],
+        port=int(os.environ["DB_PORT"]),
+        user=os.environ["DB_USER"],
+        password=os.environ["DB_PASSWORD"],
+        database=os.environ["DB_NAME"],
+        cursorclass=pymysql.cursors.DictCursor,
+    )
+
+
+def response(status_code, body):
+    return {
+        "statusCode": status_code,
+        "headers": {
+            "Content-Type": "application/json",
+            "Access-Control-Allow-Origin": "*",
+        },
+        "body": json.dumps(body),
+    }
+
+
+def lambda_handler(event, context):
+    try:
+        connection = create_connection()
+
+        with connection.cursor() as cur:
+            cur.execute(SELECT_MATCHES_SQL)
+            rows = cur.fetchall()
+
+        return response(200, rows)
+    except Exception as e:
+        traceback.print_exc()
+        return response(500, {"error": f"{e}"})
