6354838634c30d03f34df2ae2922b9f87ae70033
Nemika-Haj <nemika@bytestobits.dev>
2025-12-09 04:28:18 +0200

    Update formatted rows

--- cloudformation/lambdas/tournament-matches/get/src/app.py
+++ cloudformation/lambdas/tournament-matches/get/src/app.py
@@ -60,7 +60,7 @@ def lambda_handler(event, context):
             for row in rows
         ]
 
-        return response(200, {})
+        return response(200, formatted_rows)
     except Exception as e:
         traceback.print_exc()
         return response(500, {"error": f"{e}"})
