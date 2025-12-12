88e4e8615645494fd991608e51ebfe5c52d7eae5
Nemika-Haj <nemika@bytestobits.dev>
2025-12-13 01:00:11 +0200

    Add processed match data and player name on riot accounts get

---
+++ cloudformation/lambdas/riot-accounts/get/app.py
@@ -0,0 +1,44 @@
+import pymysql
+import os
+import json
+
+SELECT_ACCOUNTS_SQL = """
+    SELECT
+        acc.*,
+        p.name,
+        (
+            SELECT COUNT(*)
+            FROM processed_match_data pmd
+            WHERE pmd.account_puuid = acc.account_puuid
+        ) as processed_matches
+    FROM riot_accounts acc
+    JOIN players p on p.id = acc.player_id
+"""
+
+
+def create_connection() -> pymysql.Connection:
+    return pymysql.connect(
+        host=os.environ["DB_HOST"],
+        port=int(os.environ["DB_PORT"]),
+        user=os.environ["DB_USER"],
+        password=os.environ["DB_PASSWORD"],
+        database=os.environ["DB_NAME"],
+        cursorclass=pymysql.cursors.DictCursor,
+    )
+
+
+def lambda_handler(event, context):
+    conn = create_connection()
+
+    with conn.cursor() as cur:
+        cur.execute(SELECT_ACCOUNTS_SQL)
+        rows = cur.fetchall()
+
+        return {
+            "statusCode": 200,
+            "headers": {
+                "Content-Type": "application/json",
+                "Access-Control-Allow-Origin": "*",
+            },
+            "body": json.dumps({"items": rows, "count": len(rows)}),
+        }
