18fe8737595f388b266ca6f860c599a3e6ecfcca
Sam Crerar <44707197+Sam-Crerar@users.noreply.github.com>
2025-12-29 15:09:05 +0000

    WIP

---
+++ cloudformation/lambdas/champ-select-websockets/connect/src/app.py
@@ -0,0 +1,32 @@
+import boto3
+from typing import Any, Dict
+
+ddb_client = boto3.client("dynamodb")
+
+def lambda_handler(event, context):
+    try:
+        connection_id = event["requestContext"]["connectionId"]
+
+        item = {
+            "connectionId": {"S": connection_id},
+        }
+
+        put_item(
+            table_name="Connections",
+            item=item
+        )
+
+    except Exception as e:
+        error_type = type(e).__name__ 
+        error_message = str(e) 
+        print(f"Error occurred: {error_type}: {error_message}") 
+        return { "statusCode": 500, "body": f"{error_type}: {error_message}" }
+    
+    return {"statusCode": 200}
+
+
+def put_item(table_name: str, item: Dict[str, Any]) -> None:
+    ddb_client.put_item(
+        TableName=table_name,
+        Item=item
+    )
---
+++ cloudformation/lambdas/champ-select-websockets/disconnect/src/app.py
@@ -0,0 +1,50 @@
+import json
+import boto3
+from typing import Any, Dict
+
+ddb_client = boto3.client("dynamodb")
+
+
+def lambda_handler(event: Dict[str, Any], context: Any) -> Dict[str, int]:
+    
+    print("event : " + json.dumps(event))
+    
+    endpoint_url = "https://" + "/".join([
+        event["requestContext"]["domainName"],
+        event["requestContext"]["stage"]
+    ])
+
+    print("endpoint_url : " + endpoint_url)
+
+    connection_ids = scan_table("Connections")
+
+    print("connection_ids : " + str(connection_ids))
+
+    apigateway_client = boto3.client(
+        "apigatewaymanagementapi",
+        endpoint_url=endpoint_url
+    )
+
+    for item in connection_ids.get("Items", []):
+        print("item : " + str(item))
+        body = json.loads(event["body"])
+        message = body["message"]
+        send_message(
+            apigateway_client,
+            item["connectionId"]["S"],
+            message
+        )
+
+    return {"statusCode": 200}
+
+
+def scan_table(table_name: str) -> Dict[str, Any]:
+    response = ddb_client.scan(TableName=table_name)
+    return response
+
+
+def send_message(apigateway_client, connection_id: str, message: str) -> None:
+    response = apigateway_client.post_to_connection(
+        Data=message.encode("utf-8"),
+        ConnectionId=connection_id
+    )
