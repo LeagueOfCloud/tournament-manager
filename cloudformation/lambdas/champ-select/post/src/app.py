6705f464fe95a28c831f8b334084d6dc8a478531
Nemika-Haj <nemika@bytestobits.dev>
2025-12-30 14:18:46 +0200

    Fix

--- cloudformation/lambdas/champ-select/post/src/app.py
+++ cloudformation/lambdas/champ-select/post/src/app.py
@@ -7,30 +7,27 @@ from typing import Any, Dict
 
 ddb_client = boto3.client("dynamodb")
 
+
 def lambda_handler(event, context):
 
     lobby_id = f"LOBBY#{str(uuid.uuid4())}"
 
     item = {
-        "LobbyId": lobby_id,
-        "Red_Captain": "",
-        "Blue_Captain": "",
-        "Spectators": [""]
+        "lobbyId": {"S": lobby_id},
+        "redCaptain": {"S": ""},
+        "blueCaptain": {"S": ""},
+        "spectators": {"S": "[]"},
     }
 
-    put_item(
-        table_name=os.environ["TABLE_NAME"],
-        item=item
-    )
+    put_item(table_name=os.environ["TABLE_NAME"], item=item)
 
     return response(200, {"lobbyId": lobby_id})
-    
+
+
 def put_item(table_name: str, item: Dict[str, Any]) -> None:
 
-    ddb_client.put_item(
-        TableName=table_name,
-        Item=item
-    )
+    ddb_client.put_item(TableName=table_name, Item=item)
+
 
 def response(status_code, body):
     return {
