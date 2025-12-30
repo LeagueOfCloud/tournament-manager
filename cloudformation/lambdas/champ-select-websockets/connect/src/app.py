381e35f51ece88eeac1278f215cead92d1aa7dde
Sam Crerar <44707197+Sam-Crerar@users.noreply.github.com>
2025-12-30 12:50:39 +0000

    Lobby_time

--- cloudformation/lambdas/champ-select-websockets/connect/src/app.py
+++ cloudformation/lambdas/champ-select-websockets/connect/src/app.py
@@ -1,22 +1,46 @@
 import os
 import boto3
+import json
 from typing import Any, Dict
 
 ddb_client = boto3.client("dynamodb")
 
 def lambda_handler(event, context):
     try:
+        qparams = event.get("queryStringParameters") or {}
+
+        lobby_id = qparams.get("lobbyid")
+        team_type = qparams.get("teamtype") 
         connection_id = event["requestContext"]["connectionId"]
 
-        item = {
-            "connectionId": {"S": connection_id},
-        }
+        if not lobby_id:
+            return { "statusCode": 400, "body": "No Lobby Id passed" }
+        
+        lobby = get_lobby(
+            table_name=os.environ["TABLE_NAME"],
+            lobby_id=lobby_id
+        )
+
+        if not lobby:
+            return { "statusCode": 400, "body": "No Lobby found" }
+
+        blue_captain = lobby.get("blueCaptain", {}).get("S")
+        red_captain = lobby.get("redCaptain", {}).get("S")
+        spectators = json.loads(lobby.get("spectators", {}).get("S", "[]"))
+
+        if team_type == "blue" and not blue_captain:
+            lobby["blueCaptain"] = {"S": connection_id}
+        elif team_type == "red" and not red_captain:
+            lobby["redCaptain"] = {"S": connection_id}
+        else:
+            spectators.append(connection_id)
+            lobby["spectators"] = {"S": json.dumps(spectators)}
 
         put_item(
             table_name=os.environ["TABLE_NAME"],
-            item=item
+            item=lobby
         )
-
+        
     except Exception as e:
         error_type = type(e).__name__ 
         error_message = str(e) 
@@ -31,3 +55,12 @@ def put_item(table_name: str, item: Dict[str, Any]) -> None:
         TableName=table_name,
         Item=item
     )
+
+def get_lobby(table_name: str, lobby_id: str) -> Dict[str, Any]:
+    response = ddb_client.get_item(
+        TableName=table_name,
+        Key={
+            "lobbyId": {"S": lobby_id}
+        }
+    )
+    return response.get("Item", {})
